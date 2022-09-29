6b3de2a4f7d84c9da4f7a4c5a03de6b8e73e8960
chendj89 <chendj89@hotmail.com>
2022-09-29 22:12:44 +0800

    564656

--- test/cli_gitdownload.test.ts
+++ test/cli_gitdownload.test.ts
@@ -1,10 +1,8 @@
 import { describe, test, expect } from "vitest";
 import getDownload from "../playground/cli/gitdownload/dist";
-import shell from "shelljs";
 describe("cli", () => {
   test("测试1", () => {
     let ans: any = getDownload();
     expect(ans.code).toBe(0);
-    shell.rm("-rf", "./cli");
   });
 });
