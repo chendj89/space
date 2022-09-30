e4d416947926922391fb5487c1274f11c109f8a4
chendj89 <chendj89@hotmail.com>
2022-09-30 10:50:49 +0800

    64646

--- rollup/plugins/rollup-plugin-pkg.js
+++ rollup/plugins/rollup-plugin-pkg.js
@@ -1,18 +1,20 @@
 import path from "path";
 import fs from "fs";
 import basePkg from "../../bulid/pkg/base.json";
-import { dependencies } from "../../cli/package.json";
+import { dependencies, name, version } from "../../cli/package.json";
 export default function pkg({ input, dest }) {
   return {
     name: "pkg",
     buildEnd(error) {
       let dir = path.join(__dirname, "../playground/cli/toNum/package.json");
-      let updateData = {
-        name: `@chencc`,
-        version: "1.0.0",
-        repo: "",
-      };
+      // 版本+1
+      let arr = version.split(".");
+      let num = parseInt(arr.pop()) + 1;
+      arr.push(num);
+      basePkg.version = arr.join(".");
+      basePkg.name = `@chencc/${name}`;
       basePkg.dependencies = dependencies;
+      basePkg.repository.url = `https://github.com/chendj89/${name}.git`;
 
       fs.writeFileSync(dir, JSON.stringify(basePkg, null, 2), "utf-8");
     },
