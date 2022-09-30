084c9add96577c6b701dd3b7e3f21c93c9377228
chendj89 <chendj89@hotmail.com>
2022-09-30 12:56:15 +0800

    9877

---
+++ packages/gitdownload/readme.md
@@ -0,0 +1,31 @@
+#### gitdownload
+
+封装 git clone --depth 1
+
+#### 安装
+
+```bash
+npm i @chencc/gitdownload
+```
+
+##### require 模式
+
+```js
+const git = require("@chencc/gitdownload");
+git();
+```
+
+#### import 模式
+
+```ts
+import git from "@chencc/gitdownload";
+git();
+```
+
+#### tsconfig
+
+```json
+{
+  "types": ["@chencc/gitdownload"]
+}
+```
--- rollup/plugins/rollup-plugin-pkg.js
+++ rollup/plugins/rollup-plugin-pkg.js
@@ -14,7 +14,9 @@ export default function pkg({ input, dest }) {
       let src = path.dirname(path.join(__dirname, `${input}`));
       // 来源目录中的package.json
       let pkg = path.join(src, "package.json");
-
+      let readme = path.join(src, "readme.md");
+      // 确保目标目录存在
+      fs.ensureDirSync(path.join(__dirname, dest));
       // 判断是否存在
       if (fs.existsSync(pkg)) {
         let pkgJson = fs.readFileSync(pkg, "utf-8");
@@ -29,10 +31,14 @@ export default function pkg({ input, dest }) {
         basePkg.dependencies = dependencies;
         basePkg.repository.url = `https://github.com/chendj89/${name}.git`;
         basePkg.scripts.push = "npm publish";
-        // 确保目标目录存在
-        fs.ensureDirSync(path.join(__dirname, dest));
         let dir = path.join(__dirname, `${dest}/package.json`);
         fs.writeFileSync(dir, JSON.stringify(basePkg, null, 2), "utf-8");
+      }
+      // 说明文件
+      if (fs.existsSync(readme)) {
+        let readmeContent = fs.readFileSync(readme, "utf-8");
+        let dir = path.join(__dirname, `${dest}/readme.md`);
+        fs.writeFileSync(dir, readmeContent, "utf-8");
       }
     },
   };
