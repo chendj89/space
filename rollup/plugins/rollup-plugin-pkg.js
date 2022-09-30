d946b214b904fe95f882c40a629aa2a7068f5872
chendj89 <chendj89@hotmail.com>
2022-09-30 12:03:58 +0800

    676543

--- rollup/plugins/rollup-plugin-pkg.js
+++ rollup/plugins/rollup-plugin-pkg.js
@@ -1,22 +1,36 @@
 import path from "path";
 import fs from "fs";
-import basePkg from "../../bulid/pkg/base.json";
-import { dependencies, name, version } from "../../cli/package.json";
+import basePkg from "../pkg/base.json";
+/**
+ * 打包
+ * @param {*} param
+ * @returns
+ */
 export default function pkg({ input, dest }) {
   return {
     name: "pkg",
     buildEnd(error) {
-      let dir = path.join(__dirname, "../playground/cli/toNum/package.json");
-      // 版本+1
-      let arr = version.split(".");
-      let num = parseInt(arr.pop()) + 1;
-      arr.push(num);
-      basePkg.version = arr.join(".");
-      basePkg.name = `@chencc/${name}`;
-      basePkg.dependencies = dependencies;
-      basePkg.repository.url = `https://github.com/chendj89/${name}.git`;
-      basePkg.scripts.push = "npm publish";
-      fs.writeFileSync(dir, JSON.stringify(basePkg, null, 2), "utf-8");
+      // 来源目录
+      let src = path.dirname(path.join(__dirname, `${input}`));
+      // 来源目录中的package.json
+      let pkg = fs.readFileSync(path.join(src, "package.json"));
+      // 判断是否存在
+      if (fs.existsSync(pkg)) {
+        let pkgJson = fs.readFileSync(pkg, "utf-8");
+        pkgJson = JSON.parse(pkgJson);
+        const { dependencies, name, version } = pkgJson;
+        // 版本+1
+        let arr = version.split(".");
+        let num = parseInt(arr.pop()) + 1;
+        arr.push(num);
+        basePkg.version = arr.join(".");
+        basePkg.name = `@chencc/${name}`;
+        basePkg.dependencies = dependencies;
+        basePkg.repository.url = `https://github.com/chendj89/${name}.git`;
+        basePkg.scripts.push = "npm publish";
+        let dir = path.join(__dirname, `${dest}/package.json`);
+        fs.writeFileSync(dir, JSON.stringify(basePkg, null, 2), "utf-8");
+      }
     },
   };
 }
